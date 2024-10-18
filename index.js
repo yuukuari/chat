ccdf2b418ada6d81ac7af01845eb39304501550f
yuukuari <jonathancambot@hotmail.com>
2024-10-18 20:15:47 +0200

    sdfs

--- index.js
+++ index.js
@@ -6,23 +6,30 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Servir les fichiers statiques (HTML, CSS, JS) à partir du dossier "public"
 app.use(express.static(__dirname + '/public'));
 
+// Gérer les connexions WebSocket
 io.on('connection', (socket) => {
     console.log('Un utilisateur s\'est connecté');
 
-    // Lorsqu'un message est reçu de l'un des clients
-    socket.on('chatMessage', (msg) => {
-        // Envoyer le message à tous les clients
-        io.emit('chatMessage', msg);
+    // Lorsque le serveur reçoit un message depuis un client
+    socket.on('chatMessage', (data) => {
+        // Diffuser le message à tous les clients, incluant le nom de l'utilisateur
+        io.emit('chatMessage', {
+            name: data.name,
+            message: data.message
+        });
     });
 
+    // Lorsque l'utilisateur se déconnecte
     socket.on('disconnect', () => {
         console.log('Un utilisateur s\'est déconnecté');
     });
 });
 
-const PORT = 3000;
+// Démarrer le serveur sur le port 3000
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Serveur démarré sur le port ${PORT}`);
 });
